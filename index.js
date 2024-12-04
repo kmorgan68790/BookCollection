6df7e8cb2fc4b566810faefa234ac7f331b91bbd
Kelly Morgan <coco68790@gmail.com>
2024-12-03 21:57:03 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import "dotenv/config";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "books",
+  password: process.env.TOKEN,
+  port: 5432,
+});
+db.connect();
+
+app.addListener(port, (req, res) => { 
+    console.log(`Server is running on http://localhost:${port}`);
+});
