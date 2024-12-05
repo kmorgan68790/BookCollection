1d64fb434261deaabcce4dbe3daf6fe639a2d1ad
Kelly Morgan <coco68790@gmail.com>
2024-12-05 18:43:54 -0500

    added bootstraps header and footer, completed about me page, added routes for home, about, and contact

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ const db = new pg.Client({
 });
 db.connect();
 
-app.addListener(port, (req, res) => { 
+const API_URL = "https://openlibrary.org/dev/docs/api/covers";
+
+//get home page
+app.get("/", (req, res) => { 
+  res.render("home.ejs");
+});
+
+//get about page
+app.get("/about", (req, res) => { 
+  const API = API_URL;
+  res.render("about.ejs", {API : API});
+});
+
+app.listen(port, (req, res) => { 
     console.log(`Server is running on http://localhost:${port}`);
 });
