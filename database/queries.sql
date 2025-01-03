e078e59b239c8bb99a448019afd3e3f1c270f0b5
kjones2027 <kjones2027@outlook.com>
2025-01-02 21:53:08 -0500

    added more views, fixed routes issues, updated sql insert, added styles

--- database/queries.sql
+++ database/queries.sql
@@ -4,6 +4,7 @@ CREATE TABLE books (
 	id SERIAL PRIMARY KEY,
 	book_cover TEXT,
 	title TEXT NOT NULL,
+	isbn VARCHAR(25) NOT NULL,
 	date_read DATE NOT NULL DEFAULT CURRENT_DATE,
 	rating INTEGER NOT NULL,
 	book_link VARCHAR(255),
@@ -16,10 +17,12 @@ CREATE TABLE notes (
 	book_id INTEGER REFERENCES books(id)
 );
 
-INSERT INTO books (book_cover, title, date_read, rating , book_link, summary)
+INSERT INTO books (book_cover, title, isbn, date_read, rating , book_link, summary)
 VALUES 
-('https://covers.openlibrary.org/b/ISBN/9780306846335-L.jpg', 'Embrace the Suck', '2020-04-15', 5,
-'https://openlibrary.org/works/OL22166372W/Embrace_the_Suck?edition=key%3A/books/OL30203799M', 'An important and fascinating book. The author shares the hardships of his Navy Seal training and experience as a guide to empower and equip us for the challenges and decisions we face in our own lives. He does a wonderful job getting the reader to shift their perspective and take effective action. There is also a nice addition of humor. In summary if you have aspirations to do challenging yet rewarding work, this is a serious read.');
+('https://covers.openlibrary.org/b/ISBN/9780306846335-M.jpg', 'Embrace the Suck', '9780306846335', '2020-04-15', 5,
+'https://openlibrary.org/works/OL22166372W/Embrace_the_Suck?edition=key%3A/books/OL30203799M', 'An important and fascinating book. The author shares the hardships of his Navy Seal training and experience as a guide to empower and equip us for the challenges and decisions we face in our own lives. He does a wonderful job getting the reader to shift their perspective and take effective action. There is also a nice addition of humor. In summary if you have aspirations to do challenging yet rewarding work, this is a serious read.'),
+('', 'Mini Habits', '9781494882273', '2020-04-15', 5,
+'https://openlibrary.org/books/OL26396947M/Mini_Habits', 'This book is all about starting small to reach big goals. Since reading this book, I’ve adapted several mini habits like: 1. Coding for 30 minutes everyday (since practice 30 minutes always seemed like a mountain of a task) 2. Write 1 journal page a day (since 200 and even 2000 seemed overwhelming) 3. Read two pages a day (since read for one hour before bed feels daunting at the end of the day) and 4. Exercise or do 20 minutes of yoga everyday');
 
 INSERT INTO notes ( note, book_id) 
 VALUES 	
--- index.js
+++ index.js
@@ -20,16 +20,57 @@ const db = new pg.Client({
 db.connect();
 
 const API_URL = "https://openlibrary.org/dev/docs/api/covers";
+let bookList = [];
 
 //get home page
-app.get("/", (req, res) => { 
-  res.render("home.ejs");
+app.get("/", async (req, res) => { 
+  try {
+    const result = await db.query("SELECT * FROM books;");
+    const apiResult = await axios.get(API_URL);
+    // const isbnResult = await db.query("SELECT isbn FROM books WHERE id =", result.rows.id);
+    let bookList = [];
+    result.rows.forEach(book => {
+      bookList.push(book);
+    });
+    console.log(result.rows);
+
+  console.log(bookList);
+    res.render("home.ejs", {books: bookList}); 
+  } catch (err) { 
+    console.log(err);
+    res.render("home.ejs", {error: "There are currently no books to view"});
+  }
+});
+
+//get single book page
+app.get("/book/:id", async (req, res) => { 
+  const id = req.params.id;
+  try {
+    const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+    const notesResult = await db.query("SELECT note FROM notes WHERE book_id = $1", [id]);
+    console.log(result.rows[0]);
+    console.log(notesResult.rows[0]);
+    res.render("book.ejs", {book: result.rows[0], note : notesResult.rows[0]}); 
+  } catch (err) { 
+    console.log(err);
+    res.render("book.ejs", { error: "There are currently no books to view" });
+  }
+});
+
+//get edit 
+app.get("/edit", async (req, res) => {
+ res.render("edit.ejs");
 });
 
 //get about page
 app.get("/about", (req, res) => { 
   const API = API_URL;
   res.render("about.ejs", {API : API});
+});
+
+//get contact
+app.get("/contact", (req,res) => {
+  res.render("contact.ejs");
 });
 
 app.listen(port, (req, res) => { 
